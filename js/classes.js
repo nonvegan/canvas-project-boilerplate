4aa584b0c87e11be205f442b057024229c417c0c
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-02-18 16:19:37 +0000

    removed useless method from the ant class

--- js/classes.js
+++ js/classes.js
@@ -20,32 +20,10 @@ class Vector {
 }
 
 class Ant {
-  constructor(pos, size,vel) {
+  constructor(pos, size, vel) {
     this.pos = new Vector(pos.x, pos.y);
     this.size = new Vector(size.x, size.y);
-    this.vel = new Vector(vel.x,vel.y);
-  }
-  setDirection(dir) {
-    switch (dir) {
-      case "UP":
-        this.vel.x = 0;
-        this.vel.y = -this.size.y;
-        break;
-      case "RIGHT":
-        this.vel.x = this.size.x;
-        this.vel.y = 0;
-        break;
-      case "DOWN":
-        this.vel.x = 0;
-        this.vel.y = this.size.y;
-        break;
-      case "LEFT":
-        this.vel.x = -this.size.x;
-        this.vel.y = 0;
-        break;
-      default:
-        console.error("No direction");
-    }
+    this.vel = new Vector(vel.x, vel.y);
   }
   getGridCords(width, height) {
     return new Vector(this.pos.x / this.size.x, this.pos.y / this.size.y);
